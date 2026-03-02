d4376c4c717bf7e58afcd66b8ee471e4fdebeb53
Yaroslav <kovalevski.yaroslav@gmail.com>
2026-03-02 12:11:26 +0300

    Make enum more complex

--- src/main/java/com/yaroslav/ticket_booking_system/model/OrderStatus.java
+++ src/main/java/com/yaroslav/ticket_booking_system/model/OrderStatus.java
@@ -1,8 +1,20 @@
 package com.yaroslav.ticket_booking_system.model;
 
+import java.util.Set;
+
 public enum OrderStatus {
-    CREATED,
-    PAID,
-    CANCELLED,
-    REFUNDED
+    CANCELLED(Set.of()),
+    REFUNDED(Set.of()),
+    PAID(Set.of(REFUNDED)),
+    CREATED(Set.of(PAID, CANCELLED));
+
+    private final Set<OrderStatus> allowedNext;
+
+    OrderStatus(Set<OrderStatus> allowedNext) {
+        this.allowedNext = allowedNext;
+    }
+
+    boolean canTransitionTo(OrderStatus next) {
+        return !allowedNext.contains(next);
+    }
 }
