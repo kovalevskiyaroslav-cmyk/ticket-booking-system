d17a4bf5677b002dd190473deafc98f57905483d
Yaroslav <kovalevski.yaroslav@gmail.com>
2026-03-02 12:08:51 +0300

    Add exception

---
+++ src/main/java/com/yaroslav/ticket_booking_system/exception/OrderTransactionException.java
@@ -0,0 +1,8 @@
+package com.yaroslav.ticket_booking_system.exception;
+
+public class OrderTransactionException extends RuntimeException {
+
+    public OrderTransactionException(String message) {
+        super(message);
+    }
+}
