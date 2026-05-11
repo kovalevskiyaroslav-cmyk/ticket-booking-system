06a66dc96906d74a10da41d89e622c0eac6a3361
Yaroslav <kovalevski.yaroslav@gmail.com>
2026-05-11 12:32:42 +0300

    Add CORS

---
+++ src/main/java/com/yaroslav/ticket_booking_system/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.yaroslav.ticket_booking_system.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:5173",
+                        "https://ticket-booking-client-phhj.onrender.com"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
